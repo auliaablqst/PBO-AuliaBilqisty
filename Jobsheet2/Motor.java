d6b8da62fc21309a7bb67411be108c4ec2aed8af
auliaablqst <ab.auliaablqs@gmail.com>
2026-09-07 15:26:46 +0700

    feat(praktikum-03): tambahkan validasi batas kecepatan maksimal

--- Jobsheet2/Motor.java
+++ Jobsheet2/Motor.java
@@ -35,8 +35,9 @@ public class Motor {
     public void setKecepatan(int kecepatan) {
         if (!this.statusMesin && kecepatan > 0) {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        }
-        else {
+        } else if (kecepatan > 100) {
+            System.out.println("Kecepatan melebihi batas maksimal (100)!");
+        } else {
             this.kecepatan = kecepatan;
         }
     }
